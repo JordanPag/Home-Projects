4e165957073182307a6a1688484c94ca6f0df5e7
JordanPag <jpags2938@gmail.com>
2016-09-09 14:34:06 -0400

    Update PLAY..js

--- PLAY..js
+++ PLAY..js
@@ -2,6 +2,8 @@ var hospital = ["row"];
 hospital.push("floor");
 hospital.push("rowend");
 
+$("div.grid").append("<div class='floor'></div>");
+
 for (y=0;y<(hospital.length);y++) {
   if (hospital[y]=="row") {
     $("div.grid").append("<div class=row>");
