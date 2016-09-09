83f3bbfc2d13be926e78580581fb997937bc2c72
JordanPag <jpags2938@gmail.com>
2016-09-09 14:02:30 -0400

    Update PLAY..js

--- PLAY..js
+++ PLAY..js
@@ -1,1 +1,14 @@
+var hospital = ["row"];
+for (x=0;x<10;x++) {
+  hospital.push("floor");
+}
 
+for (x=0;x<hospital.length;x++) {
+  if (hospital[x]=="row") {
+    $("div.grid").append("<div class=row>");
+  } else if (hospital[x]=="rowend") {
+    $("div.grid").append("</div>");
+  } else {
+    $("div.grid").append("<div class='"+hospital[x]+"'></div>");
+  }
+}
