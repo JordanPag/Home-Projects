4756a533d70c1a0f238859bb1a324c13f9b9fcca
JordanPag <JordanPag@users.noreply.github.com>
2016-12-13 20:55:17 -0500

    Update PLAYgame.js

--- PLAYgame.js
+++ PLAYgame.js
@@ -1,1 +1,19 @@
 console.log("yay");
+var place = 100;
+
+function displayunicode(y) {
+	var unicode = y.keyCode;
+	if (unicode == 37) {
+    //left arrow
+	}
+  if (unicode == 39) {
+    //right arrow
+	}
+  if (unicode == 38) {
+    //up arrow
+	}
+  if (unicode == 40) {
+    //down arrow
+	}
+}
+
